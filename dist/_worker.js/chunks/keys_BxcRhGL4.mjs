cdc311c7fa9f4af7110ab2cc1bc00955f9092ee8
Mike Ayers <mike@vesiron.com>
2026-02-11 14:46:34 +0000

    Remove build artifacts from repo (.astro, dist)

--- dist/_worker.js/chunks/keys_BxcRhGL4.mjs
+++
@@ -1,6 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-const keyMenu = (venueId) => `menu:${venueId}`;
-const keyOrder = (orderId) => `order:${orderId}`;
-const keyOrdersIndex = (venueId) => `orders:${venueId}`;
-
-export { keyOrder as a, keyOrdersIndex as b, keyMenu as k };
--- dist/_worker.js/pages/_image.astro.mjs
+++
@@ -1,3 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { a as page } from '../chunks/image-endpoint_BDIz1t71.mjs';
-export { renderers } from '../renderers.mjs';
